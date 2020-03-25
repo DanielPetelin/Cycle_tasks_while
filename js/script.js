5020575de0d45f2e45af65ca51aaf47bd00bb7ab
DanielPetelin <daniilpetelin77@gmail.com>
2020-03-25 12:33:57 +0300

    second

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ while (k < 5) {
     k++;
     console.log('k: ' + k); 
 };
+
+// 0 + ... + 10
+let sum = 0;
+let p = 0;
+
+while (p <= 10) {
+    sum = sum + p;
+    p++
+}
+
+console.log('sum = ' + sum);
+
+// ****
+// ****
+// ****
+let out = document.querySelector('#out');
+p = 0;
+let outStr = '';
+
+while (p < 4) {
+    let p1 = 0;
+    while (p1 < 4) {
+        outStr += '*';
+        p1++;
+    }
+    outStr += '<br>';
+    p++;
+}
+
+out.innerHTML = outStr;
